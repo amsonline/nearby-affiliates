2c67c74906f007bec99645b5efb2889d2fd5ad93
Ahmad Sadeghi <allaboutams@gmail.com>
2021-12-09 05:02:48 +0330

    Tidying up the core functionality

--- app/Services/Affiliates/Affiliates.php
+++ app/Services/Affiliates/Affiliates.php
@@ -3,6 +3,10 @@
 namespace App\Services\Affiliates;
 
 class Affiliates {
+    const COMPANY_LATITUDE = 0.9308544006774926;      // Company latitude in radians (53.3340285deg)
+    const COMPANY_LONGITUDE = -0.10914502871144513;   // Company longitude in radians (-6.2535495deg)
+    const EARTH_RADIUS = 6356752.3142;                // Earth radius in meters, we can change it to kilometers to get the result in kilometers
+    const MINIMUM_DISTANCE = 100000;                  // Minimum distance in meters
 
     public function getNearbyAffiliatesFromArray(array $affiliatesList) {
         $nearbyAffiliates = [];
@@ -24,20 +28,17 @@ class Affiliates {
 	
 	private function isWithinRadius($affiliate) {
 		$distance = $this->getGreatCircleDistance($affiliate->latitude, $affiliate->longitude);
-		return ($distance <= 100000);
+		return ($distance <= self::MINIMUM_DISTANCE);
 	}
 	
 	public function getGreatCircleDistance($latitude, $longitude) {
 		$latitude = deg2rad($latitude);
 		$longitude = deg2rad($longitude);
-		$corporationLatitude = deg2rad(53.3340285);
-		$corporationLongitude = deg2rad(-6.2535495);
-		$earthRadius = 6356752.3142;
-		$deltaLatitude = abs($corporationLatitude - $latitude);
-		$deltaLongitude = abs($corporationLongitude - $longitude);
+		$deltaLatitude = abs(self::COMPANY_LATITUDE - $latitude);
+		$deltaLongitude = abs(self::COMPANY_LONGITUDE - $longitude);
 		
 		// The great circle distance formula below
-		$centralAngle = asin(sqrt(pow(sin($deltaLatitude / 2), 2) + (1 - pow(sin($deltaLatitude / 2), 2) - pow(sin(($latitude + $corporationLatitude) / 2), 2)) * pow(sin($deltaLongitude / 2), 2))) * 2;
-		return $earthRadius * $centralAngle;
+		$centralAngle = asin(sqrt(pow(sin($deltaLatitude / 2), 2) + (1 - pow(sin($deltaLatitude / 2), 2) - pow(sin(($latitude + self::COMPANY_LATITUDE) / 2), 2)) * pow(sin($deltaLongitude / 2), 2))) * 2;
+		return self::EARTH_RADIUS * $centralAngle;
 	}
 }
